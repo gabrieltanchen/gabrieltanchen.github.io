77769cfacdd913ff763f69d16db5d3785c0be1e9
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-10-16 01:14:16 -0400

    Initial redesign

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,38 @@
-$(document).foundation()
+$(document).foundation();
 
-$('.nav-menu-icon').click(function() {
-  $('.nav-drawer-container').css({
-    'display': 'block',
-  }).animate({
-    'opacity': 1,
-  }, {
-    'duration': 500,
-  });
-  $('.nav-drawer').animate({
-    'left': '0',
-  }, {
-    'duration': 500,
-  });
-});
+// $(window).scroll(function(){
+//   $('.hero-image').css('opacity', 1 - $(window).scrollTop() / 1000);
+// });
 
-$('.nav-drawer-container').click(function() {
-  $(this).animate({
-    'opacity': 0,
-  }, {
-    complete: function() {
-      $(this).css({
-        'display': 'none',
-      });
-    },
-    'duration': 500,
-  });
-  $('.nav-drawer').animate({
-    'left': '-300px',
-  }, {
-    'duration': 500,
-  });
-});
+// $('.nav-menu-icon').click(function() {
+//   $('.nav-drawer-container').css({
+//     'display': 'block',
+//   }).animate({
+//     'opacity': 1,
+//   }, {
+//     'duration': 500,
+//   });
+//   $('.nav-drawer').animate({
+//     'left': '0',
+//   }, {
+//     'duration': 500,
+//   });
+// });
+//
+// $('.nav-drawer-container').click(function() {
+//   $(this).animate({
+//     'opacity': 0,
+//   }, {
+//     complete: function() {
+//       $(this).css({
+//         'display': 'none',
+//       });
+//     },
+//     'duration': 500,
+//   });
+//   $('.nav-drawer').animate({
+//     'left': '-300px',
+//   }, {
+//     'duration': 500,
+//   });
+// });
