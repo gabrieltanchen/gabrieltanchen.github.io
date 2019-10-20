3d68a1f4a47174e8a363431da795b620ddbed59f
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-10-19 20:16:01 -0400

    lightbox

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,29 @@
 $(document).foundation();
+
+$(document).ready(function() {
+  $('.gallery-image-button').click(function() {
+    $('.lightbox-image').attr('src', $(this).attr('data-image'));
+
+    $(this).attr('data-image');
+    $('.lightbox-overlay').css({
+      display: 'block',
+    }).animate({
+      opacity: 1,
+    }, {
+      duration: 1000,
+    });
+  });
+
+  $('.lightbox-overlay').click(function() {
+    $(this).animate({
+      opacity: 0,
+    }, {
+      complete: function() {
+        $(this).css({
+          'display': 'none',
+        });
+      },
+      duration: 1000,
+    });
+  });
+});
